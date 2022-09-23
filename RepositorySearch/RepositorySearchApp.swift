90df92756e6ba81edf3004988f298d8239124220
TAAT <20purpleblue10@gmail.com>
2022-09-23 20:14:09 +0900

    Initial Commit

---
+++ RepositorySearch/RepositorySearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepositorySearchApp.swift
+//  RepositorySearch
+//
+//  Created by 董 亜飛 on 2022/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RepositorySearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
